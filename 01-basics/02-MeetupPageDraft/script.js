ec16aaad209376a64f6ec59fb6f52bcb4297b1d2
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-10-16 18:56:07 +0300

    Added task Meetup Page Draft

---
+++ 01-basics/02-MeetupPageDraft/script.js
@@ -0,0 +1,66 @@
+import Vue from './vue.esm.browser.js';
+
+/** URL адрес API */
+const API_URL = 'https://course-vue.javascript.ru/api';
+
+/** ID митапа для примера; используйте его при получении митапа */
+const MEETUP_ID = 6;
+
+/**
+ * Возвращает ссылку на изображение митапа для митапа
+ * @param meetup - объект с описанием митапа (и параметром meetupId)
+ * @return {string} - ссылка на изображение митапа
+ */
+function getMeetupCoverLink(meetup) {
+  return `${API_URL}/images/${meetup.imageId}`;
+}
+
+/**
+ * Словарь заголовков по умолчанию для всех типов элементов программы
+ */
+const agendaItemTitles = {
+  registration: 'Регистрация',
+  opening: 'Открытие',
+  break: 'Перерыв',
+  coffee: 'Coffee Break',
+  closing: 'Закрытие',
+  afterparty: 'Afterparty',
+  talk: 'Доклад',
+  other: 'Другое',
+};
+
+/**
+ * Словарь иконок для для всех типов элементов программы.
+ * Соответствует имени иконок в директории /assets/icons
+ */
+const agendaItemIcons = {
+  registration: 'key',
+  opening: 'cal-sm',
+  talk: 'tv',
+  break: 'clock',
+  coffee: 'coffee',
+  closing: 'key',
+  afterparty: 'cal-sm',
+  other: 'cal-sm',
+};
+
+export const app = new Vue({
+  el: '#app',
+
+  data: {
+    //
+  },
+
+  mounted() {
+    // Требуется получить данные митапа с API
+  },
+
+  computed: {
+    //
+  },
+
+  methods: {
+    // Получение данных с API предпочтительнее оформить отдельным методом,
+    // а не писать прямо в mounted()
+  },
+});
