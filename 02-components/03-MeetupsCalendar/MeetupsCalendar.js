59e5c3bb447cf10f717dc9c1313273733c054a9e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-11-09 11:43:59 +0300

    Added task MeetupsCalendar

---
+++ 02-components/03-MeetupsCalendar/MeetupsCalendar.js
@@ -0,0 +1,42 @@
+/*
+  Полезные функции по работе с датой можно описать вне Vue компонента
+ */
+
+export const MeetupsCalendar = {
+  name: 'MeetupsCalendar',
+
+  template: `<div class="rangepicker">
+    <div class="rangepicker__calendar">
+      <div class="rangepicker__month-indicator">
+        <div class="rangepicker__selector-controls">
+          <button class="rangepicker__selector-control-left"></button>
+          <div>Июнь 2020</div>
+          <button class="rangepicker__selector-control-right"></button>
+        </div>
+      </div>
+      <div class="rangepicker__date-grid">
+        <div class="rangepicker__cell rangepicker__cell_inactive">28</div>
+        <div class="rangepicker__cell rangepicker__cell_inactive">29</div>
+        <div class="rangepicker__cell rangepicker__cell_inactive">30</div>
+        <div class="rangepicker__cell rangepicker__cell_inactive">31</div>
+        <div class="rangepicker__cell">
+          1
+          <a class="rangepicker__event">Митап</a>
+          <a class="rangepicker__event">Митап</a>
+        </div>
+        <div class="rangepicker__cell">2</div>
+        <div class="rangepicker__cell">3</div>
+      </div>
+    </div>
+  </div>`,
+
+  // Пропсы
+
+  // В качестве локального состояния требуется хранить что-то,
+  // что позволит определить текущий показывающийся месяц.
+  // Изначально должен показываться текущий месяц
+
+  // Вычислимые свойства помогут как с получением списка дней, так и с выводом информации
+
+  // Методы понадобятся для переключения между месяцами
+};
