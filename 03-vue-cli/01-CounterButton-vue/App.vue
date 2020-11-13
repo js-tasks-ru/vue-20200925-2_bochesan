d4cd23e35a5c47ffa5168a91cc6fe7c7e5bd6167
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-11-13 15:59:10 +0300

    Added task CounterButton.vue

---
+++ 03-vue-cli/01-CounterButton-vue/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <p>
+      <counter-button v-model="count1"></counter-button> - count1 = {{ count1 }}
+    </p>
+    <p>
+      <counter-button v-model="count1"></counter-button> - count1 = {{ count1 }}
+    </p>
+    <p>
+      <counter-button v-model="count2"></counter-button> - count2 = {{ count2 }}
+    </p>
+    <p>
+      <counter-button
+        :count="count3"
+        @increment="count3 = $event"
+      ></counter-button>
+      - count3 = {{ count3 }}
+    </p>
+  </div>
+</template>
+
+<script>
+import CounterButton from './components/CounterButton';
+
+export default {
+  name: 'App',
+  components: { CounterButton },
+  data() {
+    return {
+      count1: 15,
+      count2: undefined,
+      count3: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
