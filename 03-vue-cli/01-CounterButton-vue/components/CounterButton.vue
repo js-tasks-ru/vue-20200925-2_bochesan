9b9388afb38b92de583b0607556a3c4b4440c6c7
Pavel Zabolotny <p.zabolotny@bi.zone>
2020-11-19 16:35:02 +0300

    03-vue-cli/01-CounterButton-vue

--- 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
+++ 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,33 @@
-<template><div></div></template>
+<template>
+  <button type="button" @click="changeCount">
+    {{ count }}
+  </button>
+</template>
 
 <script>
-export default {};
+export default {
+  name: 'CounterButton',
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+  methods: {
+    changeCount() {
+      this.$emit('increment', this.count + 1);
+    },
+  },
+  beforeMount: function () {
+    if (typeof this.count !== 'number' || this.count === 0) {
+      this.$emit('increment', 0);
+    }
+  },
+};
 </script>
 
 <style></style>
