a4bdfab4d92a20fdfa8fd0ed0fd6788a34f0b8a3
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-11-20 17:06:19 +0300

    Added task FormGroup

---
+++ 03-vue-cli/02-FormGroup/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app" class="page container">
+    <form-group>
+      <div class="input-group">
+        <input class="form-control" placeholder="No label" />
+      </div>
+    </form-group>
+
+    <hr />
+
+    <form-group label="Label Text">
+      <div class="input-group">
+        <input class="form-control" placeholder="With label" />
+      </div>
+    </form-group>
+
+    <hr />
+
+    <p>
+      <form-group inline>
+        <div class="input-group">
+          <input class="form-control" placeholder="Inline" />
+        </div>
+      </form-group>
+      <form-group inline>
+        <div class="input-group">
+          <input class="form-control" placeholder="Inline" />
+        </div>
+      </form-group>
+    </p>
+  </div>
+</template>
+
+<script>
+import FormGroup from './components/FormGroup';
+
+export default {
+  name: 'App',
+
+  components: { FormGroup },
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    success() {
+      this.$refs.toaster.success('Success ' + new Date().toTimeString());
+    },
+    error() {
+      this.$refs.toaster.error('Error ' + new Date().toTimeString());
+    },
+  },
+};
+</script>
+
+<style scoped>
+p,
+hr {
+  margin: 1rem auto;
+}
+
+.form-control {
+  padding: 12px 16px;
+  height: 52px;
+  border-radius: 8px;
+  border: 2px solid var(--blue-light);
+  font-family: 'Nunito', sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 28px;
+  color: var(--body-color);
+  transition: 0.2s all;
+  background-color: var(--white);
+  outline: none;
+  box-shadow: none;
+}
+
+.form-control::placeholder {
+  font-weight: 400;
+  color: var(--blue-2);
+}
+
+.form-control:focus {
+  border-color: var(--blue);
+}
+
+.input-group .form-control {
+  width: 100%;
+}
+</style>
---
+++ 03-vue-cli/02-FormGroup/components/FormGroup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="form-group">
+    <label class="form-label">Label</label>
+    <!-- <input /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormGroup',
+};
+</script>
+
+<style scoped>
+.form-group {
+  position: relative;
+  margin-bottom: 24px;
+}
+
+.form-group.form-group_inline {
+  display: inline-block;
+  margin-bottom: 0;
+}
+
+.form-group.form-group_inline + .form-group.form-group_inline {
+  margin-left: 16px;
+}
+
+.form-label {
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  color: var(--body-color);
+  margin-bottom: 10px;
+  display: block;
+}
+</style>
