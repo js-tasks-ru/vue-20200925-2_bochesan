5e17ce618b43dda3f9eea4de7bce042a561b7858
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-11-25 14:49:32 +0300

    Added task AppToast

---
+++ 03-vue-cli/03-AppToast/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="page container">
+    <app-toast ref="toaster" />
+    <p>
+      <button @click="success">Add Success Message</button>
+      <button @click="error">Add Error Message</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import AppToast from './components/AppToast';
+
+export default {
+  name: 'App',
+
+  components: { AppToast },
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    success() {
+      this.$refs.toaster.success('Success ' + new Date().toLocaleTimeString());
+    },
+    error() {
+      this.$refs.toaster.error('Error ' + new Date().toLocaleTimeString());
+    },
+  },
+};
+</script>
+
+<style scoped>
+p {
+  margin: 1rem auto;
+}
+</style>
---
+++ 03-vue-cli/03-AppToast/components/AppToast.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="toasts">
+    <div class="toast toast_success">
+      <app-icon icon="check-circle" />
+      <span>Success</span>
+    </div>
+    <div class="toast toast_error">
+      <app-icon icon="alert-circle" />
+      <span>Error</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppIcon from './AppIcon';
+
+const DELAY = 5000;
+
+export default {
+  name: 'AppToast',
+
+  components: { AppIcon },
+
+  methods: {
+    error(message) {},
+
+    success(message) {},
+  },
+};
+</script>
+
+<style scoped>
+.toasts {
+  position: fixed;
+  bottom: 8px;
+  right: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  white-space: pre-wrap;
+  z-index: 999;
+}
+
+.toast {
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  background: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  width: auto;
+}
+
+.toast + .toast {
+  margin-top: 20px;
+}
+
+.toast > .icon {
+  margin-right: 12px;
+}
+
+.toast.toast_success {
+  color: var(--green);
+}
+
+.toast.toast_error {
+  color: var(--red);
+}
+
+@media all and (min-width: 992px) {
+  .toasts {
+    bottom: 72px;
+    right: 112px;
+  }
+}
+</style>
