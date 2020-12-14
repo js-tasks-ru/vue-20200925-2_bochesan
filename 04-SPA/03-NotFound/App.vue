090fff438922dc78a5620915f42a369bd165b631
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-12-14 14:40:03 +0300

    Added task 404 - NotFound

---
+++ 04-SPA/03-NotFound/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/page-a">/page-a</router-link></li>
+          <li><router-link to="/page-b">/page-b</router-link></li>
+          <li><router-link to="/page-unknown">/page-unknown</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <main><router-view /></main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped></style>
---
+++ 04-SPA/03-NotFound/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import PageA from '../views/PageA';
+import PageB from '../views/PageB';
+import NotFoundPage from '../views/NotFoundPage';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  base: '/04-SPA/03-NotFound',
+  routes: [
+    {
+      path: '/page-a',
+      alias: '/',
+      component: PageA,
+    },
+    {
+      path: '/page-b',
+      component: PageB,
+    },
+  ],
+});
