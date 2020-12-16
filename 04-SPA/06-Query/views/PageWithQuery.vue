4cc968f0cff5dbb60bf2975ca067af345ad87619
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-12-16 16:49:42 +0300

    Added task Работа с query параметрами

---
+++ 04-SPA/06-Query/views/PageWithQuery.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <meetups-view />
+  </div>
+</template>
+
+<script>
+import MeetupsView from '../components/MeetupsView';
+
+export default {
+  name: 'PageWithQuery',
+  components: { MeetupsView },
+};
+</script>
+
+<style scoped></style>
