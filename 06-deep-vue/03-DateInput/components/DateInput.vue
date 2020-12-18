156c6e73a40dddbddd452468ba0af6335813eee3
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-12-19 00:17:35 +0300

    Added task DateInput

---
+++ 06-deep-vue/03-DateInput/components/DateInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppInput>
+    <!-- Так можно передать все слоты в дочерний компонент -->
+    <template v-for="slot of Object.keys($slots)" v-slot:[slot]>
+      <slot :name="slot" />
+    </template>
+  </AppInput>
+</template>
+
+<script>
+import AppInput from './AppInput';
+
+export default {
+  name: 'DateInput',
+
+  components: { AppInput },
+
+  props: {
+    // Чтобы value не было в списке $attrs, его можно указать в списке параметров
+    value: {},
+  },
+};
+</script>
+
+<style scoped></style>
