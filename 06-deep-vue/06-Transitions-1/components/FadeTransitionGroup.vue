fd70faa2d4c82f1b15cf46980ab28fe1faf9956e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-12-22 13:45:45 +0300

    Added task Transitions-1

---
+++ 06-deep-vue/06-Transitions-1/components/FadeTransitionGroup.vue
@@ -0,0 +1,32 @@
+<template></template>
+
+<script>
+export default {
+  name: 'FadeTransitionGroup',
+};
+</script>
+
+<style scoped>
+.fade-list {
+  position: relative;
+}
+
+.fade-list > * {
+  transition: all 0.3s ease-out;
+}
+
+.fade-list-leave-active {
+  position: absolute !important;
+  left: 0;
+  right: 0;
+}
+
+.fade-list-enter,
+.fade-list-leave-to {
+  opacity: 0;
+}
+
+.fade-list-move {
+  transition: transform 0.3s;
+}
+</style>
