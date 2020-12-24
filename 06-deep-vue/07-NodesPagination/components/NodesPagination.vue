dfc93536a9e06ec9ace5ecc409f6a369c4a75182
Pavel Zabolotny <p.zabolotny@bi.zone>
2020-12-24 16:50:53 +0300

    first variant

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,33 @@
 <script>
 export default {
   name: 'NodesPagination',
+
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+    perPage: {
+      type: Number,
+      defult: 3,
+    },
+  },
+
+  computed: {
+    viewSlots() {
+      return {
+        keyStart: this.page * this.perPage - this.perPage,
+        keyEnd: this.page * this.perPage,
+      };
+    },
+  },
+
+  render: function (createElement) {
+    return createElement(
+      'div',
+      this.$slots.default.filter(slot => slot.key > this.viewSlots.keyStart && slot.key <= this.viewSlots.keyEnd),
+    );
+  },
 };
 </script>
 
