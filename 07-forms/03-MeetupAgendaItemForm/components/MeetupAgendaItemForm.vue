b0f89c3ae08419c36b68d4ce26c2799d40af9e8c
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-12-31 00:11:30 +0300

    Added task MeetupAgendaItemForm

---
+++ 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="form-section form-section_inner">
+    <button type="button" class="remove-button">
+      <img src="../assets/icons/icon-trash.svg" alt="trash" />
+    </button>
+
+    <div class="form-group">
+      <select class="form-control" title="Тип">
+        <option value="talk">Доклад</option>
+        <option value="other">Другое</option>
+      </select>
+    </div>
+
+    <div class="form__row">
+      <div class="form__col">
+        <div class="form-group">
+          <label class="form-label">Начало</label>
+          <input class="form-control" type="time" placeholder="00:00" />
+        </div>
+      </div>
+      <div class="form__col">
+        <div class="form-group">
+          <label class="form-label">Окончание</label>
+          <input class="form-control" type="time" placeholder="00:00" />
+        </div>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="form-label">Заголовок</label>
+      <input class="form-control" />
+    </div>
+    <div class="form-group">
+      <label class="form-label">Описание</label>
+      <textarea class="form-control"></textarea>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Язык</label>
+      <select class="form-control"></select>
+    </div>
+  </div>
+</template>
+
+<script>
+const getAgendaItemTypes = () => [
+  { value: 'registration', text: 'Регистрация' },
+  { value: 'opening', text: 'Открытие' },
+  { value: 'break', text: 'Перерыв' },
+  { value: 'coffee', text: 'Coffee Break' },
+  { value: 'closing', text: 'Закрытие' },
+  { value: 'afterparty', text: 'Afterparty' },
+  { value: 'talk', text: 'Доклад' },
+  { value: 'other', text: 'Другое' },
+];
+
+const getTalkLanguages = () => [
+  { value: null, text: 'Не указано' },
+  { value: 'RU', text: 'RU' },
+  { value: 'EN', text: 'EN' },
+];
+
+export default {
+  name: 'MeetupAgendaItemForm',
+};
+</script>
+
+<style></style>
---
+++ 07-forms/03-MeetupAgendaItemForm/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/styles/app.css';
+import './assets/styles/_forms.css';
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
