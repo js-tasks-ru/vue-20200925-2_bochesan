9f99db0e2702dcbcbb227746ec100c8eee262f2e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-01-20 16:00:34 +0300

    Added task GeneratedForm

---
+++ 07-forms/04-GeneratedForm/components/DropdownButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <select @change="$emit('change', $event.target.value)">
+    <option
+      v-for="option in options"
+      :value="option.value"
+      :key="option.value"
+      :selected="option.value === value"
+    >
+      {{ option.text }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'DropdownButton',
+
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    value: String,
+    title: String,
+  },
+
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+};
+</script>
+
+<style></style>
---
+++ 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="form-section form-section_inner">
+    <button type="button" class="remove-button">
+      <img src="../assets/icons/icon-trash.svg" alt="trash" />
+    </button>
+
+    <div class="form-group">
+      <dropdown-button title="Тип" :options="$options.agendaItemTypes" />
+    </div>
+
+    <div class="form__row">
+      <div class="form__col">
+        <div class="form-group">
+          <label class="form-label">Начало</label>
+          <app-input type="time" placeholder="00:00" />
+        </div>
+      </div>
+      <div class="form__col">
+        <div class="form-group">
+          <label class="form-label">Окончание</label>
+          <app-input type="time" placeholder="00:00" />
+        </div>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="form-label">Заголовок</label>
+      <app-input />
+    </div>
+    <div class="form-group">
+      <label class="form-label">Описание</label>
+      <app-input multiline />
+    </div>
+    <div class="form-group">
+      <label class="form-label">Язык</label>
+      <dropdown-button
+        :options="[
+          { value: null, text: 'Не указано' },
+          { value: 'RU', text: 'RU' },
+          { value: 'EN', text: 'EN' },
+        ]"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppInput from './AppInput';
+import DropdownButton from './DropdownButton';
+import {
+  getAgendaItemsFieldSpecifications,
+  getAgendaItemTypes,
+} from '../meetup-service';
+
+export default {
+  name: 'MeetupAgendaItemForm',
+
+  components: { AppInput, DropdownButton },
+
+  agendaItemTypes: getAgendaItemTypes(),
+  fieldSpecifications: getAgendaItemsFieldSpecifications(),
+};
+</script>
+
+<style></style>
